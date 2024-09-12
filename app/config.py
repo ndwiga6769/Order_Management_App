d25eadbc35fe2e6e6033d35d36d9b80fd13e0751
ndwiga6769 <your.email@example.com>
2024-09-12 17:33:28 +0300

    codes

---
+++ app/config.py
@@ -0,0 +1,6 @@
+class Config:
+    SECRET_KEY = 'supersecretkey'
+    JWT_SECRET_KEY = 'anothersecretkey'
+    DATABASE = 'ecommerce.db'
+    AFRICAS_TALKING_USERNAME = 'sandbox'
+    AFRICAS_TALKING_API_KEY = 'your_api_key'
